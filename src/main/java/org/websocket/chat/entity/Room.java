4c59b88b4ac36e915d36b997969450f26a7c9cdd
confusedGustas <gustas.antanas2003@gmail.com>
2025-02-18 19:49:05 +0200

    .env file, README.md

--- src/main/java/org/websocket/chat/entity/Room.java
+++ src/main/java/org/websocket/chat/entity/Room.java
@@ -13,7 +13,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
